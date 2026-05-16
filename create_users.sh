6d8de16678eb429df5b59305e0a999f6bebd8aab
musabahmed517-cyber <musabahmed517@gmail.com>
2026-05-16 16:17:49 +0200

    Add bash shell to user creation script

--- create_users.sh
+++ create_users.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 
 # Kontrollera att scriptet körs som root
@@ -10,15 +11,17 @@ fi
 for user in "$@"; do
   echo "Skapar användare: $user"
 
-  # Skapa användare med hemkatalog
-  useradd -m "$user" 2>/dev/null
+  # Skapa användare med hemkatalog och bash-shell
+  useradd -m -s /bin/bash "$user"
 
   # Skapa mappar
-  mkdir -p /home/$user/Documents
-  mkdir -p /home/$user/Downloads
-  mkdir -p /home/$user/Work
-  chmod 700 /home/$user/Documents
-chmod 700 /home/$user/Downloads
-chmod 700 /home/$user/Work
+  mkdir -p "/home/$user/Documents"
+  mkdir -p "/home/$user/Downloads"
+  mkdir -p "/home/$user/Work"
+
+  # Sätt rättigheter
+  chmod 700 "/home/$user/Documents"
+  chmod 700 "/home/$user/Downloads"
+  chmod 700 "/home/$user/Work"
 
 done
