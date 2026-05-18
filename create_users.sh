0333f9ce5c751e192155c46ca1da3709c3820bb3
musabahmed517-cyber <musabahmed517@gmail.com>
2026-05-18 15:24:12 +0200

    Update create_users.sh

--- create_users.sh
+++ create_users.sh
@@ -1,35 +1,40 @@
 #!/bin/bash
 
-# Kontrollera root
+# Kontrollera att scriptet körs som root
 if [ "$EUID" -ne 0 ]; then
   echo "Du måste köra detta script som root"
   exit 1
 fi
 
-# Loopa igenom användare
+# Loopa igenom alla användare som skickas in
 for user in "$@"; do
 
-  # Skapa användare om den inte finns
+  # Skapa användare om den inte redan finns
   if ! id "$user" &>/dev/null; then
     useradd -m "$user"
   fi
 
-  # Skapa mappar
+  # Skapa kataloger
   mkdir -p "/home/$user/Documents"
   mkdir -p "/home/$user/Downloads"
   mkdir -p "/home/$user/Work"
 
-  # Sätt ägare
+  # Sätt rätt ägare
   chown -R "$user:$user" "/home/$user"
 
-  # Rättigheter
+  # Sätt rättigheter
   chmod 700 "/home/$user/Documents"
   chmod 700 "/home/$user/Downloads"
   chmod 700 "/home/$user/Work"
 
-  # Welcome-fil
+  # Skapa welcome.txt
   echo "Välkommen $user" > "/home/$user/welcome.txt"
 
-  # Rättigheter på filen
+  # Lista alla användare i systemet
+  cut -d: -f1 /etc/passwd >> "/home/$user/welcome.txt"
+
+  # Rättigheter på welcome-filen
   chown "$user:$user" "/home/$user/welcome.txt"
   chmod 600 "/home/$user/welcome.txt"
+
+done
