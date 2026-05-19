d902504805849f9601e601fdbbac948887ed77f3
musabahmed517-cyber <musabahmed517@gmail.com>
2026-05-19 14:49:10 +0200

    Update create_users.sh

--- create_users.sh
+++ create_users.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
-# Kontrollera att scriptet kan köra admin-kommandon
+# Kontrollera att scriptet körs som root
 if [ "$EUID" -ne 0 ]; then
-  if sudo -n true 2>/dev/null; then
-    SUDO="sudo"
-  else
-    echo "Du måste köra detta script som root"
-    exit 1
-  fi
-else
-  SUDO=""
+  echo "Du måste köra detta script som root"
+  exit 1
 fi
 
 # Loopa igenom alla användare
@@ -17,22 +11,28 @@ for user in "$@"; do
 
   # Skapa användaren om den inte finns
   if ! id "$user" &>/dev/null; then
-    $SUDO useradd -m "$user"
+    useradd -m "$user"
   fi
 
   # Skapa mappar
-  $SUDO mkdir -p "/home/$user/Documents" "/home/$user/Downloads" "/home/$user/Work"
+  mkdir -p "/home/$user/Documents"
+  mkdir -p "/home/$user/Downloads"
+  mkdir -p "/home/$user/Work"
 
-  # Sätt ägare och rättigheter
-  $SUDO chown -R "$user:$user" "/home/$user"
-  $SUDO chmod 700 "/home/$user/Documents" "/home/$user/Downloads" "/home/$user/Work"
+  # Sätt ägare
+  chown -R "$user:$user" "/home/$user"
+
+  # Sätt rättigheter
+  chmod 700 "/home/$user/Documents"
+  chmod 700 "/home/$user/Downloads"
+  chmod 700 "/home/$user/Work"
 
   # Skapa welcome.txt
-  echo "Välkommen $user" | $SUDO tee "/home/$user/welcome.txt" >/dev/null
-  cut -d: -f1 /etc/passwd | $SUDO tee -a "/home/$user/welcome.txt" >/dev/null
+  echo "Välkommen $user" > "/home/$user/welcome.txt"
+  cut -d: -f1 /etc/passwd >> "/home/$user/welcome.txt"
 
-  $SUDO chown "$user:$user" "/home/$user/welcome.txt"
-  $SUDO chmod 600 "/home/$user/welcome.txt"
+  # Rättigheter för welcome.txt
+  chmod 600 "/home/$user/welcome.txt"
 
 done
 
